f8ffabb420bac168eaa3ff2b60953925c0f7c1b8
Maddis87 <madeleneamberman@gmail.com>
2018-05-16 15:16:10 +0200

    a .sh file the main script for installing apache and wordpress

---
+++ installApacheWordpress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#A script that installs Apache2 if it's not already installed
+
+source funcApacheWordpress.sh
+
+header "Updating"
+
+sudo apt-get update
+
+header "Apache"
+
+installApache2
+
+
